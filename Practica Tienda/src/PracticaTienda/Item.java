595d83ce77ec605dda3cbfe0c0cf6a3463cddb67
danms07 <dan.ms.0739@gmail.com>
2019-02-22 09:32:26 -0600

    Cambios en la api

---
+++ Practica Tienda/src/PracticaTienda/Item.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticaTienda;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author dany
+ */
+public class Item implements Serializable{
+    
+    private String pic;
+    private String name;
+    private int stock;
+    
+    public Item(){
+        
+    }
+    
+}
